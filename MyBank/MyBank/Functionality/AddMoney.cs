dccbc645f3490d31ab9d26714c64ad17d5bc53b2
luke-d-moore <luke.d.moore1@gmail.com>
2024-04-19 12:47:30 +0100

    changes to add more tests
    changes to add more tests and mock results from the getaccount call

--- MyBank/MyBank/Functionality/AddMoney.cs
+++ MyBank/MyBank/Functionality/AddMoney.cs
@@ -16,6 +16,15 @@
             if(amount <= 0) throw new ArgumentException("Amount must be greater than 0");
             if(accountNumber <= 0) throw new ArgumentException("AccountNumber must be greater than 0");
             if(string.IsNullOrEmpty(sortCode)) throw new ArgumentException("SortCode must not be null or empty");
+            if(accountNumber.ToString().Length != 8) throw new InvalidDataException($"AccountNumber must be 8 digits in length");
+            if(sortCode.ToString().Length != 8) throw new InvalidDataException($"SortCode must be 8 characters in length");
+
+            var account = _accountDataStore.GetAccount(accountNumber, sortCode);
+
+            if (account == null) 
+                throw new InvalidDataException($"Account Could not be found with AccountNumber : {accountNumber} and SortCode : {sortCode}");
+
+
 
             return true;
         }
--- MyBank/MyBankTests/AddMoneyTests.cs
+++ MyBank/MyBankTests/AddMoneyTests.cs
@@ -5,17 +5,27 @@ namespace MyBankTests
 {
     public class AddMoneyTests
     {
-        private readonly IAccountDataStore _accountDataStore;
+        private readonly Mock<IAccountDataStore> _accountDataStore;
         private readonly IAddMoney _addMoney;
         public AddMoneyTests()
         {
-            _accountDataStore = new Mock<IAccountDataStore>().Object;
-            _addMoney = new AddMoney(_accountDataStore);
+            _accountDataStore = new Mock<IAccountDataStore>();
+            _addMoney = new AddMoney(_accountDataStore.Object);
         }
+
         [Fact]
         public void AddMoney_ValidAmount_ReturnsTrue()
         {
             //Arrange
+            var account = new Account() 
+            { 
+                AccountNumber = 12345678, 
+                SortCode = "12-34-56", 
+                Balance = 1000, 
+                Withdrawn = 0, 
+                PaidIn = 0 
+            };
+            _accountDataStore.Setup(x => x.GetAccount(12345678, "12-34-56")).Returns(account);
             var result = _addMoney.AddFunds(12345678, "12-34-56", 5);
             //Act and Assert
             Assert.True(result);
@@ -36,6 +46,41 @@ namespace MyBankTests
         {
             // Arrange
             var exceptionType = typeof(ArgumentException);
+            // Act and Assert
+            Assert.Throws(exceptionType, () => _addMoney.AddFunds(AccountNumber, SortCode, Amount));
+        }
+
+        public static IEnumerable<object[]> InvalidData =>
+        new List<object[]>
+        {
+            new object[] { 12345678, "123456", 5 },
+            new object[] { 12345678, "invalid", 5 },
+            new object[] { 1, "12-34-56", 5 },
+            new object[] { 123456789, "12-34-56", 5 }
+        };
+
+        [Theory, MemberData(nameof(InvalidData))]
+        public void AddMoney_InvalidData_ThrowsDataException(long AccountNumber, string SortCode, decimal Amount)
+        {
+            // Arrange
+            var exceptionType = typeof(InvalidDataException);
+            // Act and Assert
+            Assert.Throws(exceptionType, () => _addMoney.AddFunds(AccountNumber, SortCode, Amount));
+        }
+
+        public static IEnumerable<object[]> InvalidAccountData =>
+        new List<object[]>
+        {
+            new object[] { 11111111, "99-99-99", 5 },
+            new object[] { 99999999, "11-11-11", 5 }
+        };
+
+        [Theory, MemberData(nameof(InvalidAccountData))]
+        public void AddMoney_InValidAccountData_ThrowsDataException(long AccountNumber, string SortCode, decimal Amount)
+        {
+            //Arrange
+            _accountDataStore.Setup(x => x.GetAccount(It.IsAny<long>(), It.IsAny<string>())).Returns(value: null);
+            var exceptionType = typeof(InvalidDataException);
             // Act and Assert
             Assert.Throws(exceptionType, () => _addMoney.AddFunds(AccountNumber, SortCode, Amount));
         }
